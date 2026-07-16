f817c2750d5cbad110d06fa02643ebc21b6e7ac3
Jaynil Bhalani <bhalanijaynil@gmail.com>
2026-07-16 18:58:16 +0530

    admin fix

--- carverse/urls.py
+++ carverse/urls.py
@@ -4,7 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.path if hasattr(admin.site, 'path') else admin.site.urls), # Standard path
+    path('admin/', admin.site.urls), # Standard path
     path('api/', include('api.urls')),
 ]
 
